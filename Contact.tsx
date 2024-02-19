54d8da09f27af541bb07d437d8c1ff654ed0ab79
Hussain Ali <iamhussainali2@gmail.com>
2024-02-20 05:04:10 +0530

    first commit

---
+++ Contact.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text, View } from "react-native"
+
+const Contact = () =>{
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Contact: 9064198653</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Contact;
