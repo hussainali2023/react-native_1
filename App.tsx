94a0372198f6d3a45b6b6502343055f0f36a55dd
Hussain Ali <iamhussainali2@gmail.com>
2024-02-19 15:53:19 +0530

    added some style

--- App.tsx
+++ App.tsx
@@ -3,20 +3,32 @@ import React from "react";
 import {
   View,
   SafeAreaView,
-  Text
+  Text,
+  StyleSheet
 } from "react-native"
 
 const App = () =>{
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
-        <Text>Hello World</Text>
-        <Text>Hello World</Text>
-        <Text>Hello World</Text>
+      <View >
+        <Text style={styles.heading}>Hello World</Text>
       </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    textAlign:"center",
+    fontSize: 30,
+    fontWeight:"900",
+    marginTop: 20,
+    color:"red",
+    backgroundColor:"yellow",
+    marginHorizontal:50,
+    padding:20,
+    borderRadius: 20
+  }
+})
 
 export default App;
